aee6766d4de51ba2b9cdb56b31ad5917bb86f1a3
李满满 <xmdevops@vip.qq.com>
2017-09-21 15:41:59 +0800

    :muscle: modify log level to WARN

--- upgradeserver_lua/settings.py
+++ upgradeserver_lua/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'upgradeserver_lua', 'static')
 
 # Logging settings
 DLOGGER = 'upgradeserver'
-DJANGO_LOG_LEVEL = logging.INFO
+DJANGO_LOG_LEVEL = logging.WARN
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
