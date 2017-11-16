6d1dd395131f2b99d0d7331d60f5b0f3b0e69428
李满满 <xmdevops@vip.qq.com>
2017-11-16 11:03:20 +0800

    :white_check_mark: add firmware cn/en commit

--- upgradeserver_lua/admin.py
+++ upgradeserver_lua/admin.py
@@ -44,7 +44,7 @@ class UpgradeLogAdmin(admin.ModelAdmin):
 class FirmwareAdmin(admin.ModelAdmin):
     ordering = ['name']
     list_per_page = 20
-    fields = ['name', 'is_important', 'date', 'notes']
+    fields = ['name', 'is_important', 'date', 'cn_commit', 'en_commit', 'notes']
     search_fields = ['name', 'notes']
     list_display = ['id', 'name', 'is_important', 'is_generated', 'date', 'notes']
 
